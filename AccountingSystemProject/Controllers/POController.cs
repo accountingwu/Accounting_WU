b9ab325d9d0aabb656e07120feb0f65ba55ce453
DESKTOP-7ONKQ8V\Wich <wichersleyer@gmail.com>
2018-02-20 10:32:07 +0700

    เพิ่มงาน ใบสั่งซื้อ,ใบขอซื้อ

---
+++ AccountingSystemProject/Controllers/POController.cs
@@ -0,0 +1,25 @@
+﻿using AccountingSystemProject.DAL;
+using AccountingSystemProject.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingSystemProject.Controllers
+{
+    public class POController : Controller
+    {
+        // GET: PO
+        public QSoft_WUEntities _db = new QSoft_WUEntities();
+
+        public ActionResult ManagePo()
+        {
+            List<PR2> purchase = _db.PR2.ToList();
+            //List<PR_member> purmem = _db.PR_member.Tolist();
+            return View();
+            
+        }
+    }
+}
