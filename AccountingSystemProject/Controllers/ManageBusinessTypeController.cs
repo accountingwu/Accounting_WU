4868b52d8673193193711a4d1ad23ca7a6363ecf
DESKTOP-TD7IJ1L\Bobsaechua <nattanan301138@gmail.com>
2018-02-22 12:53:07 +0700

    ประเภทธุรกิจ

---
+++ AccountingSystemProject/Controllers/ManageBusinessTypeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountingSystemProject.DAL;
+using PagedList;
+using AccountingSystemProject.Models;
+
+namespace AccountingSystemProject.Controllers
+{
+    public class ManageBusinessTypeController : Controller
+    {
+        // GET: ManageBusinessType
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
